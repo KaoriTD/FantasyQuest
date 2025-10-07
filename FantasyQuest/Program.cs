e7196bf50391d10d35a550346e9601984c1c0cce
Kaori Tahara <kaori.tahara@divcom.ch>
2025-10-07 18:37:42 +0200

    Ajoutez des fichiers projet.

---
+++ FantasyQuest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FantasyQuest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("█▀▀ ▄▀█ █▄░█ ▀█▀ ▄▀█ █▀ █▄█   █▀█ █░█ █▀▀ █▀ ▀█▀");
+            Console.WriteLine("█▀░ █▀█ █░▀█ ░█░ █▀█ ▄█ ░█░   ▀▀█ █▄█ ██▄ ▄█ ░█░");
+        }
+    }
+}
